92a4fad087208694cbb245bf33614175cc752f7a
Carsten Hammer <carsten.hammer@t-online.de>
2026-09-11 10:36:15 +0200

    QA: verify formatter region tests fail without fix

--- org.eclipse.jdt.core/formatter/org/eclipse/jdt/internal/formatter/linewrap/WrapPreparator.java
+++ org.eclipse.jdt.core/formatter/org/eclipse/jdt/internal/formatter/linewrap/WrapPreparator.java
@@ -1479,7 +1479,7 @@ public class WrapPreparator extends ASTVisitor {
 					next.breakBefore();
 				token = next;
 			}
-			previousRegionEnd = Math.max(0, region.getOffset() + region.getLength() - 1);
+			previousRegionEnd = region.getOffset() + region.getLength() - 1;
 		}
 	}
 
